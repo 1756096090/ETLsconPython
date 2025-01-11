d820d0d47bb39bf0dda198af078eac73584039e0
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2025-01-11 13:00:17 -0500

    poblar datos de tabla 'associations'

--- pupulate_base_startup.py
+++ pupulate_base_startup.py
@@ -2,6 +2,7 @@
 from populate_db.roles import RoleManager
 from populate_db.companies import CompanyManager
 from populate_db.users import UserManager
+from populate_db.associations import AssociationManager
 #role_manager = RoleManager('oltp')
 #role_manager.save_roles(5)
 
@@ -11,6 +12,8 @@ from populate_db.users import UserManager
 manager = UserManager('oltp')
 manager.save_users(num_users=1000)
 
+# manager = AssociationManager('oltp')
+# manager.save_associations(num_associations=50)
 
 # import populate_db.roles as roles
 
